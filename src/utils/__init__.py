6faabe867bab73360473348ed582243238ad21d8
SERRE Loïc <loic.serre@etu.univ-amu.fr>
2025-03-31 15:55:30 +0200

    Refactoring __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,7 @@
-# This file is intentionally left blank.
+# Importing main classes
+from game.board import Board
+from game.menu import Menu
+from game.rules import Rules
+
+# Importing utility functions
+from game.button import draw_back_button
