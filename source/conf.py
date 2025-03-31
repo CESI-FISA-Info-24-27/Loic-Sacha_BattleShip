ad65bada09f557614c6e9391c04ad6b496462247
Loïc SERRE <loicserre@outlook.com>
2025-04-01 00:03:00 +0200

    Add CI/CD pipeline for pytest and Sphinx #22

--- source/conf.py
+++ source/conf.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('../src'))
 
 # Configuration file for the Sphinx documentation builder.
@@ -31,5 +32,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
